813a44995a1370eb0db6156c0c0e345a94dd2edd
Chu <Chu@github.com>
2023-07-17 00:43:56 +0100

    Challenge #4

--- script.js
+++ script.js
@@ -20,7 +20,19 @@ console.log(convertDaysToAge(6570));
 const getLargestNumber = (number1, number2, number3) => {
     const calculateLargestNumber = Math.max (number1, number2, number3);
     return calculateLargestNumber;
-};
+}
 
 console.log(getLargestNumber(2,1,4));
 console.log(getLargestNumber(6,2,3));
+
+
+
+
+
+const getLastName = (array) => {
+//    let lastName = getLastName.lastIndexOf();
+    return array[array.length - 1];
+}
+console.log(getLastName(["Charlie", "Rob", "Andy"]));
+console.log(getLastName(["Ash","Stu"]));
+
