7b5e65b61b5de58054fb2b6280bf16897d7cb4bd
Chu <Chu@github.com>
2023-07-17 00:29:07 +0100

    Challenge #3

--- script.js
+++ script.js
@@ -1,18 +1,26 @@
 const isNumberPositive = (myNumber) => {
-if (myNumber > 0) {
-  console.log("false");
-} else {
-  console.log("true");
-}
-}
+  if (myNumber > 0) {
+    console.log("false");
+  } else {
+    console.log("true");
+  }
+};
 
 isNumberPositive(-1);
 isNumberPositive(10);
 
 const convertDaysToAge = (myAgeInDays) => {
-    const ageInYears = myAgeInDays / 365;
-    return ageInYears;
-}
+  const ageInYears = myAgeInDays / 365;
+  return ageInYears;
+};
 
 console.log(convertDaysToAge(3650));
 console.log(convertDaysToAge(6570));
+
+const getLargestNumber = (number1, number2, number3) => {
+    const calculateLargestNumber = Math.max (number1, number2, number3);
+    return calculateLargestNumber;
+};
+
+console.log(getLargestNumber(2,1,4));
+console.log(getLargestNumber(6,2,3));
