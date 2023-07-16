5700202a0743fad0d65a92d3ff8550261528f097
Chu <Chu@github.com>
2023-07-17 00:23:15 +0100

    Challenge #2

--- script.js
+++ script.js
@@ -8,3 +8,11 @@ if (myNumber > 0) {
 
 isNumberPositive(-1);
 isNumberPositive(10);
+
+const convertDaysToAge = (myAgeInDays) => {
+    const ageInYears = myAgeInDays / 365;
+    return ageInYears;
+}
+
+console.log(convertDaysToAge(3650));
+console.log(convertDaysToAge(6570));
