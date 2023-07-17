376cbb85f4a266e1f17452f14f9b4de4e4447651
Chu <Chu@github.com>
2023-07-17 01:07:43 +0100

    Final Commit

--- script.js
+++ script.js
@@ -18,21 +18,35 @@ console.log(convertDaysToAge(3650));
 console.log(convertDaysToAge(6570));
 
 const getLargestNumber = (number1, number2, number3) => {
-    const calculateLargestNumber = Math.max (number1, number2, number3);
-    return calculateLargestNumber;
-}
+  const calculateLargestNumber = Math.max(number1, number2, number3);
+  return calculateLargestNumber;
+};
 
-console.log(getLargestNumber(2,1,4));
-console.log(getLargestNumber(6,2,3));
-
-
-
-
+console.log(getLargestNumber(2, 1, 4));
+console.log(getLargestNumber(6, 2, 3));
 
 const getLastName = (array) => {
-//    let lastName = getLastName.lastIndexOf();
-    return array[array.length - 1];
-}
+  //    let lastName = getLastName.lastIndexOf();
+  return array[array.length - 1];
+};
 console.log(getLastName(["Charlie", "Rob", "Andy"]));
-console.log(getLastName(["Ash","Stu"]));
+console.log(getLastName(["Ash", "Stu"]));
 
+//const allNumbersPositive = (array) => {
+//    const calculateIfallArePositive = array.some(x => x < 0);
+//    return calculateIfallArePositive;
+//}
+//allNumbersPositive([2,4,5]);
+//allNumbersPositive([-5,4,6]);
+
+const allNumbersPositive = (array) => {
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] < 0) {
+      return false;
+    } else return true;
+  }
+}
+
+console.log(allNumbersPositive([2, 4, 5]));
+
+console.log(allNumbersPositive([-5, 4, 6]));
